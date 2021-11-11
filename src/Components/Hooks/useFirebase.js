af42017380d85268098174d8119e3feaccf74aab
JaznanOfficial <jaznanofficial@gmail.com>
2021-11-11 08:50:02 +0600

    register form done

--- src/Components/Hooks/useFirebase.js
+++ src/Components/Hooks/useFirebase.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import initializeConfig from '../Firebase/firebase.init';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 
 
 
 initializeConfig()
 const useFirebase = () => {
-    return {
+    
+    const auth = getAuth();
+    const [user, setUser] = useState({})
+    
 
+
+
+
+
+    const registerWithPassword = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+  
+    }
+    return {
+        registerWithPassword,
     };
 };
 
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,18 +1,53 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React, { useState } from 'react';
 import './Register.css'
 import { Link } from "react-router-dom";
+import useFirebase from '../Hooks/useFirebase';
 
 const Register = () => {
-    const { register, handleSubmit } = useForm();
-  const onSubmit = data => console.log(data);
+    const { registerWithPassword } = useFirebase();
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    const handleName = (e) => {
+        e.preventDefault();
+        setName(e.target.value);
+    }
+    const handleEmail = (e) => {
+        e.preventDefault();
+        setEmail(e.target.value);
+    }
+    const handlePassword = (e) => {
+        e.preventDefault();
+        setPassword(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(name, email, password);
+        registerWithPassword(email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            // ...
+          })
+          .catch((error) => {
+            
+            const errorMessage = error.message;
+            // ..
+          });
+    }
+    
+    
     return (
         <div className='register'>
         <h1 className='text-center text-primary my-5'>Register</h1>
-        <form onSubmit={handleSubmit(onSubmit)}>
-      <input type='text' {...register("name")} placeholder='Your Name' />
-      <input type='email' {...register("email")} placeholder='Your Email' />
-      <input type="password" {...register("password")} placeholder='Your Password' />
+        <form onSubmit={handleSubmit}>
+      <input type='text'  placeholder='Your Name' onBlur={handleName} />
+      <input type='email'  placeholder='Your Email' onBlur={handleEmail} />
+      <input type="password"  placeholder='Your Password' onBlur={handlePassword} />
       <input type="submit" />
             </form>
             <h1 className='text-center text-primary'>________________</h1>
