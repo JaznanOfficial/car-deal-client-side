978c5b13a396fc99240a51b8ee4ae8dac6f60710
JaznanOfficial <jaznanofficial@gmail.com>
2021-11-12 18:34:31 +0600

    some changes

--- src/Components/AddProducts/AddProducts.js
+++ src/Components/AddProducts/AddProducts.js
@@ -1,19 +1,33 @@
 import axios from 'axios';
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { useHistory, useLocation } from 'react-router';
+import Swal from 'sweetalert2';
 import './AddProducts.css'
 
 const AddProducts = () => {
+    
     const { register, handleSubmit } = useForm();
     const onSubmit = data => {
         
         axios.post('https://agile-anchorage-04157.herokuapp.com/addCars', data)
             .then(res => {
             console.log(res);
-        })
+            })
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'You are successfully submited your order',
+                showConfirmButton: false,
+                timer: 3000
+            })
+            
+        
     }
+    
     return (
         <div className='addCar'>
+        <h1 className='text-danger pb-0 my-3 text-center'>Add a car to sell</h1>
         <form onSubmit={handleSubmit(onSubmit)}>
         <input placeholder='Product Name' {...register("name")} />
         <input placeholder='Product Img Url' {...register("img" )} />
