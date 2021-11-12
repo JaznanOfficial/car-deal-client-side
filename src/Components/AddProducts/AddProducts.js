187ed66b05a002439a6af920b6ffba38d83375a6
JaznanOfficial <jaznanofficial@gmail.com>
2021-11-12 18:09:29 +0600

    post product api

--- src/Components/AddProducts/AddProducts.js
+++ src/Components/AddProducts/AddProducts.js
@@ -1,9 +1,26 @@
+import axios from 'axios';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import './AddProducts.css'
 
 const AddProducts = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => {
+        
+        axios.post('https://agile-anchorage-04157.herokuapp.com/addCars', data)
+            .then(res => {
+            console.log(res);
+        })
+    }
     return (
-        <div>
-            <h1>here admin will add an order or delete</h1>
+        <div className='addCar'>
+        <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("name")} />
+        <input {...register("img" )} />
+        <input {...register("details" )} />
+        <input type="number" {...register("price")} />
+        <input type="submit" />
+      </form>
         </div>
     );
 };
